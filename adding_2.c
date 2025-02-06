f9c6639bed8ce72720c08e0d2fc2283640190041
Aswanth Premkumar <aswanthpremkumar6@gmail.com>
2025-02-06 15:12:50 +0530

    level 1 problem 1

---
+++ adding_2.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a; 
+printf("enter the num;");
+ scanf("%d",&a);
+    a=a+2;
+    printf("the num is :%d",a);
+}
