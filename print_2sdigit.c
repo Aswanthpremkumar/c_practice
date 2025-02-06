5f9c2d9be28b947466ac7fd82275b790be927126
Aswanth Premkumar <aswanthpremkumar6@gmail.com>
2025-02-06 22:47:42 +0530

    level 1 problem7

---
+++ print_2sdigit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int num,digit ;
+    printf("enter 2 digit number:");
+    scanf("%d",&num);
+
+    
+        digit=num/10;
+    
+    printf("the num is:%d",digit);
+    
+}
